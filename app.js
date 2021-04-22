714cd2da63e6c2933a710a49a803a3cafe802c95
Richard.C <r.conde@live.com>
2021-04-22 12:31:38 -0400

    emptying and clearing button_container and user-input within renderButtons function

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 $(document).ready(function () {
   // Functions
-    function renderButtons(){
-        for (let i = 0; i < starterBtn.length; i++) {
-            let Btn = $("<button>")
-              .addClass("professionBtn")
-              .attr("data-Btn", starterBtn[i])
-              .text(starterBtn[i]);
-              $(".button_container").append(Btn);
-          }
-    };
-
-    
+  function renderButtons() {
+      $(".button_container").empty();
+      $("#user-input").val(" ");
+    for (let i = 0; i < starterBtn.length; i++) {
+      let Btn = $("<button>")
+        .addClass("professionBtn")
+        .attr("data-Btn", starterBtn[i])
+        .text(starterBtn[i]);
+      $(".button_container").append(Btn);
+    }
+  }
 
   // buttons Array
   const starterBtn = ["teacher", "chef", "lawyer", "dentist"];
@@ -19,11 +19,12 @@ $(document).ready(function () {
   renderButtons();
 
   // capture user input and display in btn area
-  $("#select-profession").on("click",function(){
-      event.preventDefault();
-      //console.log("clicked");
-      let userInput = $("#user-input").val();
-      //console.log(userInput);
+  $("#select-profession").on("click", function () {
+    event.preventDefault();
+    //console.log("clicked");
+    let userInput = $("#user-input").val();
+    //console.log(userInput);
+    starterBtn.push(userInput);
+    renderButtons();
   });
-
 });
