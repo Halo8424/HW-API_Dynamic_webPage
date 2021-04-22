545ba0a3bef822c4a608f7ed19fe92b77fe8e6f6
Richard.C <r.conde@live.com>
2021-04-22 11:38:46 -0400

    starterBtn array displaying on document

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
-$(document).ready(function(){
-    // buttons Array
-    const starterBtn = ["teacher","chef","lawyer","dentist"];
-        //console.log(starterBtn);
-    // loop over create btns and display to page 
+$(document).ready(function () {
+  // Functions
 
+  // buttons Array
+  const starterBtn = ["teacher", "chef", "lawyer", "dentist"];
+  //console.log(starterBtn);
+  // loop over create btns and display to page
+  for (let i = 0; i < starterBtn.length; i++) {
+    let Btn = $("<button>")
+      .addClass("professionBtn")
+      .attr("data-Btn", starterBtn[i])
+      .text(starterBtn[i]);
+      $(".button_container").append(Btn);
+  }
 });
