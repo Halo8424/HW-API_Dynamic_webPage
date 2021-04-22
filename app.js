71dc0172ac9b683ec955999d4edba35a5b000150
Richard.C <r.conde@live.com>
2021-04-22 11:22:33 -0400

    hard coded array int

---
+++ app.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    // buttons Array
+    const starterBtn = ["teacher","chef","lawyer","dentist"];
+        //console.log(starterBtn);
+    // loop over create btns and display to page 
+
+});
